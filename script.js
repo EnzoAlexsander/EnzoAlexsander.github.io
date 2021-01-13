2891fd7f08d3723ae6137d04339ddb31ade4dbcd
EnzoAlexsander <enzoalexsander9@gmail.com>
2021-01-13 12:06:47 -0300

    Script

---
+++ script.js
@@ -0,0 +1,30 @@
+let music = document.getElementById('audio');
+
+function retroceder() {
+    music.currentTime -= 15;
+}
+
+function avancar() {
+    music.currentTime += 15;
+}
+
+function diminuir_vel() {
+    music.playbackRate -= 0.1;
+}
+
+function aumentar_vel() {
+    music.playbackRate += 0.1;
+}
+
+function play() {
+    music.play();
+}
+
+function pause() {
+    music.pause();
+}
+
+function stop() {
+    music.pause();
+    music.currentTime = 0;
+}
